b1f1474f107bdf94745430c2a85255cd3eece290
Đào Huy Tuấn <tuandh95@gmail.com>
2025-05-19 00:14:13 +0700

    Delete database/migrations directory

--- database/migrations/0001_robust_sway.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "sync_state" (
-	"id" varchar(50) PRIMARY KEY NOT NULL,
-	"block_number" bigint NOT NULL
-);
--- database/migrations/0002_easy_morlun.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "sync_state" ADD COLUMN "last_transfer_id" varchar(100) NOT NULL;--> statement-breakpoint
-ALTER TABLE "sync_state" ADD COLUMN "last_account_id" varchar(66) NOT NULL;
